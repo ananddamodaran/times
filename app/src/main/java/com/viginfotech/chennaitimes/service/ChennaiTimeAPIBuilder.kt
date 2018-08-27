a9d000eeed3e2e9d0f7a115d80d79776b5540b1f
Anand Damodaran <dvlpr.anand@gmail.com>
2018-08-27 20:14:41 +0530

    URL updated

--- app/src/main/java/com/viginfotech/chennaitimes/service/ChennaiTimeAPIBuilder.kt
+++ app/src/main/java/com/viginfotech/chennaitimes/service/ChennaiTimeAPIBuilder.kt
@@ -1,17 +1,22 @@
 package com.viginfotech.chennaitimes.service
 
+import com.viginfotech.chennaitimes.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ChennaiTimeAPIBuilder {
-     companion object {
-        private const val CHENNAI_TIMES_BASE_URL="https://chennaitimes-prod.appspot.com/"
+    companion object {
+        private val CHENNAI_TIMES_BASE_URL =
+                if (BuildConfig.DEBUG) BuildConfig.BACKEND_STAGING
+                else BuildConfig.BACKEND_PROD
+
+
         private val chennaiTimesServiceBuilder = Retrofit.Builder()
                 .baseUrl(CHENNAI_TIMES_BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
 
-        private val retrofitChennaiTimesAPI= chennaiTimesServiceBuilder.build()!!
+        private val retrofitChennaiTimesAPI = chennaiTimesServiceBuilder.build()!!
 
-        fun buildChennaiTimesService()= retrofitChennaiTimesAPI.create(ChennaiTimesService::class.java)!!
+        fun buildChennaiTimesService() = retrofitChennaiTimesAPI.create(ChennaiTimesService::class.java)!!
     }
 }
